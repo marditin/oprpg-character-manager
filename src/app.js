79c6dc80e21e2ded8fa828fbae4c0db9ce6bc641
marditin <vinifos13@gmail.com>
2026-06-22 16:46:32 -0300

    feat: add cors support and middleware configuration

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import charactersRouter from './routes/characters.js';
 import equipmentsRouter from './routes/equipments.js';
+import cors from 'cors';
 import { logger } from './middleware/logger.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(logger);
 
